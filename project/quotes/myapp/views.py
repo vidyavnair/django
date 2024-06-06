2c8434c21d7cc46bcab48ea5e3586fcebc578618
vidya v nair <vidyav342@gmail.com>
2024-06-06 14:55:25 +0530

    second commit

--- project/quotes/myapp/views.py
+++ project/quotes/myapp/views.py
@@ -1,11 +1,20 @@
-import requests
-from django.shortcuts import render
+
 
 # Create your models here.
 from django.shortcuts import render
 from .models import Quote
 import requests
 
+
+
+
+def Log(request):
+    if request.method=='POST':
+        email=request.POST['name']
+        passw=request.POST['pass']
+    return render(request,"log.html")
+def register(request):
+    return render(request,"register.html")
 # for fetching random quotes from api and save it in our database Quote.
 def get_random_quote():
     response = requests.get("https://api.quotable.io/random")
@@ -20,8 +29,8 @@ def get_random_quote():
     return text, author
 
 def home(request):
-    quote, author = get_random_quote()
-    return render(request, 'home.html', {'quote': quote, 'author': author})
+    quote = Quote.objects.order_by('?').first()
+    return render(request, 'home.html', {'quote': quote.text, 'author': quote.author})
 
 def search(request):
     query = request.GET.get('q')
